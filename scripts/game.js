613dccab28deb553c034841aa9260489652a9619
NadimRifaii <nadimrifaii3@gmail.com>
2023-11-11 17:30:50 +0200

    added scripts folder with game.js and declared global variables inside of it

---
+++ scripts/game.js
@@ -0,0 +1,7 @@
+const levelTitle = document.querySelector('#level-title')
+const buttons = document.querySelectorAll('.btn')
+let mouseEventsDisabled = true
+let orderArray = []
+let level = 1
+let currentIndex = 0
+let userCanClick = false;
